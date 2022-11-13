66b92364de00d5f5d92e19996aeedbd735581639
michaelfehr <michael_fehrgmx.de>
2022-11-13 18:44:36 +0100

    Initial commit

---
+++ app/src/main/java/de/androidcrypto/chattutorialscaledrone/MainActivity.java
@@ -0,0 +1,108 @@
+package de.androidcrypto.chattutorialscaledrone;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    // replace this with a real channelID from Scaledrone dashboard
+    private String channelID = "CHANNEL_ID_FROM_YOUR_SCALEDRONE_DASHBOARD";
+    private String roomName = "observable-room";
+    private EditText editText;
+    private MessageAdapter messageAdapter;
+    private ListView messagesView;
+    MemberData memberData;
+
+    ImageButton send;
+    boolean myMessage = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        messageAdapter = new MessageAdapter(this);
+        messagesView = (ListView) findViewById(R.id.messages_view);
+        messagesView.setAdapter(messageAdapter);
+
+        send = findViewById(R.id.btnSend);
+
+        memberData = new MemberData(getRandomName(), getRandomColor());
+
+
+    }
+
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+            //scaledrone.publish(roomName, message);
+            //MemberData memberData = new MemberData();
+            Message messageClass = new Message(message, memberData, myMessage);
+            messageAdapter.add(messageClass);
+            messagesView.setSelection(messagesView.getCount() - 1);
+            editText.getText().clear();
+            myMessage = !myMessage;
+        }
+    }
+
+
+
+    private String getRandomName() {
+        String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
+        String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
+        return (
+            adjs[(int) Math.floor(Math.random() * adjs.length)] +
+            "_" +
+            nouns[(int) Math.floor(Math.random() * nouns.length)]
+        );
+    }
+
+    private String getRandomColor() {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer("#");
+        while(sb.length() < 7){
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0, 7);
+    }
+}
+
+class MemberData {
+    private String name;
+    private String color;
+
+    public MemberData(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public MemberData() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberData{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+
+Description: https://www.scaledrone.com/blog/android-chat-tutorial/
+
+Source: https://github.com/ScaleDrone/android-chat-tutorial
